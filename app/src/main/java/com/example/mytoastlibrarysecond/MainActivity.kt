82302c26b421d1ac36179913a30090923e04fdd8
faraznb <alifarazzain@gmail.com>
2021-12-09 17:17:46 +0500

    updated

--- app/src/main/java/com/example/mytoastlibrarysecond/MainActivity.kt
+++ app/src/main/java/com/example/mytoastlibrarysecond/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.mytoastlibrarysecond
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mytoast.MyToast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        this.finish()
+
+        MyToast.myToast(this, "Hekllo 'woejf")
     }
 }
